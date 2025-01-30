9c8bbd8c9c91465fe8544224149aa12f6d1b72c9
jknap <jsknap@hotmail.com>
2025-01-30 16:57:13 +0100

    fix button size

--- src/components/ui/buttons/button.tsx
+++ src/components/ui/buttons/button.tsx
@@ -25,8 +25,8 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-9 px-3 text-xs',
-        lg: 'h-11 px-8 text-base',
+        sm: 'h-8 px-2.5 text-xs',
+        lg: 'h-12 px-8 text-base',
         icon: 'h-10 w-10',
         'icon-sm': 'h-8 w-8',
         'icon-lg': 'h-12 w-12',
